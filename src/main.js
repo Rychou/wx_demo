40333ef13e031893129a2dee0f9ea469d3fff045
hellovass <hellova33@gmail.com>
2018-06-02 16:27:31 +0800

    文件目录

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import weui from './css/weui.wxss'
 
 Vue.config.productionTip = false
 App.mpType = 'app'
@@ -11,12 +12,21 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main', '^pages/index/main'],
+    pages: ['^pages/index/main'],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: '下沙读书会',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      'list': [{
+        'pagePath': 'pages/index/main',
+        'text': '首页'
+      }, {
+        'pagePath': 'pages/users/main',
+        'text': '我'
+      }]
     }
   }
 }
